01ada3b0ec34e129260f896a2de063cf2e244f41
Mark Gent <Mark.Gent@homesengland.gov.uk>
2025-09-12 16:37:55 +0100

    Docker compose hooked up to VS debugging

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+sed -i 's/\r$//' "$0"
+
 export AWS_REGION=eu-west-2
 export AWS_DEFAULT_REGION=eu-west-2
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
+
+set -e
 
 # S3 buckets
 # aws --endpoint-url=http://localhost:4566 s3 mb s3://my-bucket
