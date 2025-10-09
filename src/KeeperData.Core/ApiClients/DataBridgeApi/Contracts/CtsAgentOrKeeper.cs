aab7a52a131f4a043afb13dbdfa132d54deec4ff
Mark Gent <Mark.Gent@homesengland.gov.uk>
2025-10-09 12:49:08 +0100

    Added a full set of Bronze (Raw) models for SAM and CTS data types

--- src/KeeperData.Core/ApiClients/DataBridgeApi/Contracts/CtsAgentOrKeeper.cs
+++ src/KeeperData.Core/ApiClients/DataBridgeApi/Contracts/CtsAgentOrKeeper.cs
@@ -2,9 +2,27 @@ namespace KeeperData.Core.ApiClients.DataBridgeApi.Contracts;
 
 public class CtsAgentOrKeeper : BronzeBase
 {
-    public string PAR_ID { get; set; } = string.Empty;
+    public long PAR_ID { get; set; } = default;
+    public string LID_FULL_IDENTIFIER { get; set; } = string.Empty;
 
     public string? PAR_TITLE { get; set; }
     public string? PAR_INITIALS { get; set; }
     public string? PAR_SURNAME { get; set; }
+    public string? PAR_TEL_NUMBER { get; set; }
+    public string? PAR_MOBILE_NUMBER { get; set; }
+    public string? PAR_EMAIL_ADDRESS { get; set; }
+
+    public string? LOC_TEL_NUMBER { get; set; }
+    public string? LOC_MOBILE_NUMBER { get; set; }
+
+    public string ADR_NAME { get; set; } = string.Empty;
+    public string? ADR_ADDRESS_2 { get; set; }
+    public string? ADR_ADDRESS_3 { get; set; }
+    public string? ADR_ADDRESS_4 { get; set; }
+    public string? ADR_ADDRESS_5 { get; set; }
+    public string? ADR_POST_CODE { get; set; }
+
+    public DateTime LPR_EFFECTIVE_FROM_DATE { get; set; } = default;
+    public DateTime? LPR_EFFECTIVE_TO_DATE { get; set; }
+
 }
