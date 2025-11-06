7c4ff95071c693fc742dac2d60743cc3d9691702
Tris <tristan.sellick-taylor@esynergy.co.uk>
2025-11-06 17:18:02 +0000

    Before I forget, let's see if this fixes the wonky test

--- tests/KeeperData.Api.Tests.Component/Orchestration/Sam/Holdings/SamHoldingImportOrchestratorTests.cs
+++ tests/KeeperData.Api.Tests.Component/Orchestration/Sam/Holdings/SamHoldingImportOrchestratorTests.cs
@@ -73,6 +73,15 @@ public class SamHoldingImportOrchestratorTests
         factory.OverrideServiceAsScoped(_goldPartyRepositoryMock.Object);
         factory.OverrideServiceAsScoped(_goldSitePartyRoleRelationshipRepositoryMock.Object);
         factory.OverrideServiceAsScoped(_goldSiteGroupMarkRelationshipRepositoryMock.Object);
+        
+        // Register lookup service mocks
+        factory.OverrideServiceAsScoped(_countryIdentifierLookupServiceMock.Object);
+        factory.OverrideServiceAsScoped(_premiseActivityTypeLookupServiceMock.Object);
+        factory.OverrideServiceAsScoped(_premiseTypeLookupServiceMock.Object);
+        factory.OverrideServiceAsScoped(_productionTypeLookupServiceMock.Object);
+        factory.OverrideServiceAsScoped(_productionUsageLookupServiceMock.Object);
+        factory.OverrideServiceAsScoped(_roleTypeLookupServiceMock.Object);
+        factory.OverrideServiceAsScoped(_speciesTypeLookupServiceMock.Object);
 
         SetupDataBridgeApiRequest(factory, holdingsUri, HttpStatusCode.OK, HttpContentUtility.CreateResponseContent(holdings));
         SetupDataBridgeApiRequest(factory, holdersUri, HttpStatusCode.OK, HttpContentUtility.CreateResponseContent(holders));
