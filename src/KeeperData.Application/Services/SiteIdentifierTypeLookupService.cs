a2d93d533f977900eb203ba9a20180cd3e9d29db
Tris <tristan.sellick-taylor@esynergy.co.uk>
2025-11-13 15:59:20 +0000

    Added site identifier repo

--- src/KeeperData.Application/Services/SiteIdentifierTypeLookupService.cs
+++ src/KeeperData.Application/Services/SiteIdentifierTypeLookupService.cs
@@ -1,42 +1,19 @@
 using KeeperData.Core.Documents;
+using KeeperData.Core.Repositories;
 using KeeperData.Core.Services;
 
 namespace KeeperData.Application.Services;
 
-public class SiteIdentifierTypeLookupService : ISiteIdentifierTypeLookupService
+public class SiteIdentifierTypeLookupService(ISiteIdentifierTypeRepository siteIdentifierTypeRepository)
+    : ISiteIdentifierTypeLookupService
 {
-    /// <summary>
-    /// To complete implementation when seeding is completed or to replace.
-    /// </summary>
-    /// <param name="id"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
     public async Task<SiteIdentifierTypeDocument?> GetByIdAsync(string? id, CancellationToken cancellationToken)
     {
-        if (string.IsNullOrWhiteSpace(id)) return null;
-
-        return await Task.FromResult(new SiteIdentifierTypeDocument
-        {
-            IdentifierId = id,
-            Code = "Code",
-            Name = "Name",
-            IsActive = true
-        });
+        return await siteIdentifierTypeRepository.GetByIdAsync(id, cancellationToken);
     }
 
-    /// <summary>
-    /// To complete implementation when seeding is completed or to replace.
-    /// </summary>
-    /// <param name="lookupValue"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
     public async Task<(string? siteIdentifierId, string? siteIdentifierName)> FindAsync(string? lookupValue, CancellationToken cancellationToken)
     {
-        if (string.IsNullOrWhiteSpace(lookupValue)) return (null, null);
-
-        string? siteIdentifierId = null;
-        string? siteIdentifierName = null;
-
-        return await Task.FromResult((siteIdentifierId, siteIdentifierName));
+        return await siteIdentifierTypeRepository.FindAsync(lookupValue, cancellationToken);
     }
 }
--- src/KeeperData.Core/Documents/Reference/SiteIdentifierTypeListDocument.cs
+++ src/KeeperData.Core/Documents/Reference/SiteIdentifierTypeListDocument.cs
@@ -1,15 +1,22 @@
 using KeeperData.Core.Attributes;
+using KeeperData.Core.Repositories;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace KeeperData.Core.Documents.Reference;
 
 [CollectionName("refSiteIdentifierTypes")]
-public class SiteIdentifierTypeListDocument : IListDocument
+public class SiteIdentifierTypeListDocument : IListDocument, IReferenceListDocument<SiteIdentifierTypeDocument>
 {
+    public static string DocumentId => "all-siteidentifiertypes";
+
     [BsonId]
-    public string Id { get; set; } = "all-siteidentifiertypes";
+    public string Id { get; set; } = DocumentId;
+
+    public int? LastUpdatedBatchId { get; set; }
 
     public DateTime LastUpdatedDate { get; set; }
 
     public List<SiteIdentifierTypeDocument> SiteIdentifierTypes { get; set; } = [];
+
+    public IReadOnlyCollection<SiteIdentifierTypeDocument> Items => SiteIdentifierTypes.AsReadOnly();
 }
