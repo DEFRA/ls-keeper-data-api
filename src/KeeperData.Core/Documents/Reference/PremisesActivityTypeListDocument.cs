4f51cf86fa40490db2ad86c6b3bd9531f2eb0c22
Tris <tristan.sellick-taylor@esynergy.co.uk>
2025-11-13 15:58:08 +0000

    Added premises activity type repo

--- src/KeeperData.Core/Documents/Reference/PremisesActivityTypeListDocument.cs
+++ src/KeeperData.Core/Documents/Reference/PremisesActivityTypeListDocument.cs
@@ -1,15 +1,22 @@
 using KeeperData.Core.Attributes;
+using KeeperData.Core.Repositories;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace KeeperData.Core.Documents.Reference;
 
 [CollectionName("refPremisesActivityTypes")]
-public class PremisesActivityTypeListDocument : IListDocument
+public class PremisesActivityTypeListDocument : IListDocument, IReferenceListDocument<PremisesActivityTypeDocument>
 {
+    public static string DocumentId => "all-premisesactivitytypes";
+
     [BsonId]
-    public string Id { get; set; } = "all-premisesactivitytypes";
+    public string Id { get; set; } = DocumentId;
+
+    public int? LastUpdatedBatchId { get; set; }
 
     public DateTime LastUpdatedDate { get; set; }
 
     public List<PremisesActivityTypeDocument> PremisesActivityTypes { get; set; } = [];
+
+    public IReadOnlyCollection<PremisesActivityTypeDocument> Items => PremisesActivityTypes.AsReadOnly();
 }
