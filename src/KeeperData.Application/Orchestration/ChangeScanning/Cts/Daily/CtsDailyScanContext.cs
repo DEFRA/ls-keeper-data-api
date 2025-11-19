9d48946e327165afc52ea5c5f82253a1fa6da1b5
huss <huss.rasit@defra.gov.uk>
2025-11-19 11:49:02 +0000

    CtsDailyScanContext merge issue

--- src/KeeperData.Application/Orchestration/ChangeScanning/Cts/Daily/CtsDailyScanContext.cs
+++ src/KeeperData.Application/Orchestration/ChangeScanning/Cts/Daily/CtsDailyScanContext.cs
@@ -2,9 +2,6 @@ namespace KeeperData.Application.Orchestration.ChangeScanning.Cts.Daily;
 
 public class CtsDailyScanContext
 {
-    public DateTime CurrentDateTime { get; init; }
-    public DateTime? UpdatedSinceDateTime { get; init; }
-    public int PageSize { get; init; }
     public Guid ScanCorrelationId { get; init; } = Guid.NewGuid();
     public DateTime CurrentDateTime { get; init; } = DateTime.UtcNow;
     public DateTime? UpdatedSinceDateTime { get; init; }
