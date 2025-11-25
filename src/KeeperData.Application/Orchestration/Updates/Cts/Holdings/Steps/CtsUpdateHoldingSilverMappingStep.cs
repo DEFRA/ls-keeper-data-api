b8f81bf99a9d91399fdbd339fb80988826aa4836
huss <huss.rasit@defra.gov.uk>
2025-11-25 13:24:48 +0000

    cts holdings update

---
+++ src/KeeperData.Application/Orchestration/Updates/Cts/Holdings/Steps/CtsUpdateHoldingSilverMappingStep.cs
@@ -0,0 +1,44 @@
+using KeeperData.Application.Orchestration.Imports.Cts.Mappings;
+using KeeperData.Core.Attributes;
+using KeeperData.Core.Domain.Enums;
+using KeeperData.Core.Services;
+using Microsoft.Extensions.Logging;
+
+namespace KeeperData.Application.Orchestration.Updates.Cts.Holdings.Steps;
+
+[StepOrder(2)]
+public class CtsUpdateHoldingSilverMappingStep(
+    IRoleTypeLookupService roleTypeLookupService,
+    ILogger<CtsUpdateHoldingSilverMappingStep> logger)
+    : UpdateStepBase<CtsUpdateHoldingContext>(logger)
+{
+    protected override async Task ExecuteCoreAsync(CtsUpdateHoldingContext context, CancellationToken cancellationToken)
+    {
+        if (context.RawHolding != null)
+        {
+            context.SilverHolding = CtsHoldingMapper.ToSilver(
+                context.CurrentDateTime,
+                context.RawHolding);
+        }
+
+        context.SilverParties = [
+            .. await CtsAgentOrKeeperMapper.ToSilver(
+                context.CurrentDateTime,
+                context.RawAgents,
+                HoldingIdentifierType.CphNumber,
+                InferredRoleType.Agent,
+                roleTypeLookupService.FindAsync,
+                cancellationToken),
+
+            .. await CtsAgentOrKeeperMapper.ToSilver(
+                context.CurrentDateTime,
+                context.RawKeepers,
+                HoldingIdentifierType.CphNumber,
+                InferredRoleType.LivestockKeeper,
+                roleTypeLookupService.FindAsync,
+                cancellationToken)
+        ];
+
+        context.SilverPartyRoles = CtsPartyRoleRelationshipMapper.ToSilver(context.SilverParties);
+    }
+}
