a4df456bae3e1c26414aef44b576cf4ffa379336
Mark Gent <MarkGent1@users.noreply.github.com>
2026-01-13 14:57:39 +0000

    Create R1 and R1.1 swagger docs (#121)
    * Added r1 amd r1.1 versions for the livestock open api spec
    
    * Updated page size limitations to be 500 max.
    
    * Added ImportThrottlingSettings
    
    ---------
    
    Co-authored-by: Mark Gent <Mark.Gent@homesengland.gov.uk>

--- src/KeeperData.Application/Configuration/QueryValidationConfig.cs
+++ src/KeeperData.Application/Configuration/QueryValidationConfig.cs
@@ -6,6 +6,6 @@ public class QueryValidationConfig
 {
     public static string SectionName = "QueryValidation";
     public string? ValidatorType { get; set; }
-    public int MaxPageSize { get; set; } = 100;
+    public int MaxPageSize { get; set; } = 500;
     public int MaxQueryableTypes { get; set; } = 50;
 }
