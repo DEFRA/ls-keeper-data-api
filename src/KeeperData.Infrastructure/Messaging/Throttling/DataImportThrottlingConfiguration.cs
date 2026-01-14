1789dd2d7b064d57a4a37411247aac0cf51f860b
Mark Gent <Mark.Gent@homesengland.gov.uk>
2026-01-14 17:05:18 +0000

    Corrected naming missmatch for config values

--- src/KeeperData.Infrastructure/Messaging/Throttling/DataImportThrottlingConfiguration.cs
+++ src/KeeperData.Infrastructure/Messaging/Throttling/DataImportThrottlingConfiguration.cs
@@ -4,5 +4,7 @@ namespace KeeperData.Infrastructure.Messaging.Throttling;
 
 public class DataImportThrottlingConfiguration : IDataImportThrottlingConfiguration
 {
+    public const string SectionName = "DataImportThrottlingSettings";
+
     public int MessageCompletionDelayMs { get; set; }
 }
