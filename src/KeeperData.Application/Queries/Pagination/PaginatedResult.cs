ba5992320c3ca6e3e135a1a38a61b464532b2e64
Tris <tristan.sellick-taylor@esynergy.co.uk>
2026-02-05 17:13:16 +0000

    Added a bunch of component tests

--- src/KeeperData.Application/Queries/Pagination/PaginatedResult.cs
+++ src/KeeperData.Application/Queries/Pagination/PaginatedResult.cs
@@ -2,12 +2,13 @@ namespace KeeperData.Application.Queries.Pagination;
 
 public class PaginatedResult<T>
 {
-    public int Count { get; set; }
+    public int Count { get; set; }  // Number of items on current page
+    public int TotalCount { get; set; }  // Total number of items across all pages
     public List<T> Values { get; set; } = [];
     public int Page { get; set; }
     public int PageSize { get; set; }
 
-    public int TotalPages => (int)Math.Ceiling((double)Count / PageSize);
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
     public bool HasNextPage => Page < TotalPages;
     public bool HasPreviousPage => Page > 1;
 }
