e86c00650cd4ca3fee767521376aa3116bbbc49c
Graham McVea <graham.mcvea@bartonkeys.com>
2026-04-13 09:56:09 +0100

    Add lastModifiedDate to CountrySummaryDto
    Added a lastModifiedDate property to CountrySummaryDto to track the last update timestamp. Also reformatted the class declaration and included a summary comment for the new property.

--- src/KeeperData.Core/DTOs/CountrySummaryDto.cs
+++ src/KeeperData.Core/DTOs/CountrySummaryDto.cs
@@ -2,7 +2,7 @@ using System.Text.Json.Serialization;
 
 namespace KeeperData.Core.DTOs;
 
-public class CountrySummaryDto: CountryDTO
+public class CountrySummaryDto : CountryDTO
 {
     /// <summary>
     /// The timestamp of the last time the Country record was updated.
